3c5edb94e3dae84f621e8f39ae74ac578694d4c2
aittis-rctr <juha.aittamaa@gmail.com>
2019-01-19 15:08:34 +0200

    Initialized project and made postgresql connection

---
+++ src/main/java/com/codemoth/resourcetestapi/ResourceTestApiApplication.java
@@ -0,0 +1,14 @@
+package com.codemoth.resourcetestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceTestApiApplication.class, args);
+	}
+
+}
+
